a463dfd70f3af1e54b9be5cf1c3ad99acf885765
Michał Dziadowicz <michal@dziadowicz.it>
2023-08-07 23:03:31 +0200

    My Site: Add blog pagination

--- book-django-4-by-example/mysite/blog/views.py
+++ book-django-4-by-example/mysite/blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published_manager.all()
+    posts_list = Post.published_manager.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
