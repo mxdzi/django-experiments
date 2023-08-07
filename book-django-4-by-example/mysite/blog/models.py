bc266ccb2fdc32462ee011eacb3c99d79adc7ecf
Michał Dziadowicz <michal@dziadowicz.it>
2023-08-07 20:26:34 +0200

    My Site: Add blog seo links

--- book-django-4-by-example/mysite/blog/models.py
+++ book-django-4-by-example/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -9,13 +10,12 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
-
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='published')
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
 
@@ -34,3 +34,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.published.year, self.published.month, self.published.day, self.slug])
