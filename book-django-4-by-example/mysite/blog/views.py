9ee74d84b9b9295a2f58402f300ad4b40a820b34
Michał Dziadowicz <michal@dziadowicz.it>
2023-08-08 20:29:52 +0200

    My Site: Add blog post share

--- book-django-4-by-example/mysite/blog/views.py
+++ book-django-4-by-example/mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from blog.forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -30,3 +32,19 @@ def post_detail(request, year, month, day, post):
                              published__month=month,
                              published__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} from {cd['name']} at {post_url}, {cd['comments']}"
+            send_mail(subject, message, 'admin@example.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
--- book-django-4-by-example/mysite/mysite/settings.py
+++ book-django-4-by-example/mysite/mysite/settings.py
@@ -113,3 +113,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email server configuration
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
