15cfe24130f854acfd5ffd0a49d179d5bca6950f
Michał Dziadowicz <michal@dziadowicz.it>
2023-08-08 19:58:04 +0200

    My Site: Add blog class-based views

--- book-django-4-by-example/mysite/blog/views.py
+++ book-django-4-by-example/mysite/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published_manager.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
