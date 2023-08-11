23bfc3b9194a7bdc77d175e6395672098430a879
Michał Dziadowicz <michal@dziadowicz.it>
2023-08-11 20:13:08 +0200

    My Site: Add blog templatetags

---
+++ book-django-4-by-example/mysite/blog/templatetags/blog_tags.py
@@ -0,0 +1,21 @@
+from django import template
+from ..models import Post
+from django.db.models import Count
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published_manager.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published_manager.order_by('-published')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published_manager.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
